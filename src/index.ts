60e1d5be3e1d543593ec2963e7c6186e34853d81
Shashank kumar <shashankk204@gmail.com>
2024-12-21 16:20:48 +0530

    added CORS

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import { Hono } from 'hono'
 import Blog from './routes/Blog.route'
 import User from './routes/User.route'
 import { Auth } from './middlewares/Auth.Middleware'
+import { cors } from 'hono/cors'
 
 
 
 const app = new  Hono().basePath("/api/v1")
 
+
+app.use("/*",cors())
 app.use('/blog/*',Auth);
 
 app.route('/user',User)
